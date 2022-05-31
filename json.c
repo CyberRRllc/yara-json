401fe8f608f6932ae809229a1c6f8e7d54198b12
Michael Grant <mgrant@cyberrr.com>
2022-05-31 10:30:53 +0000

    updated for Yara 4.x

--- json.c
+++ json.c
@@ -4,6 +4,30 @@
 
 #define MODULE_NAME json
 
+int module_initialize(YR_MODULE* module) {
+    return ERROR_SUCCESS;
+}
+
+
+int module_finalize(YR_MODULE* module) {
+    return ERROR_SUCCESS;
+}
+
+
+int module_load(YR_SCAN_CONTEXT* context, YR_OBJECT* module_object, void* module_data, size_t module_data_size) {
+    YR_MEMORY_BLOCK* block = first_memory_block(context);
+    const uint8_t* block_data = block->fetch_data(block);
+
+    json_error_t json_error;
+    json_t* json = json_loads((const char*) block_data, 0, &json_error);
+    module_object->data = json;
+
+    return ERROR_SUCCESS;
+}
+
+int module_unload(YR_OBJECT* module_object) {
+    return ERROR_SUCCESS;
+}
 
 define_function(key_value) {
     json_t* json = module()->data;
@@ -16,7 +40,7 @@ define_function(key_value) {
 
     json_t* json_value = json_object_get(json, key);
     if (json_value == NULL) {
-        return_integer(UNDEFINED);
+        return_integer(YR_UNDEFINED);
     }
 
     const char* json_val = json_string_value(json_value);
@@ -50,12 +74,13 @@ define_function(has_key_r) {
         return ERROR_INVALID_FILE;
     }
 
-    RE_CODE key_regex = regexp_argument(1);
+    YR_SCAN_CONTEXT* context = scan_context();
+    RE* key_regex = regexp_argument(1);
 
     void *iter = json_object_iter(json);
     while (iter) {
         const char *json_key = json_object_iter_key(iter);
-        if (yr_re_match(key_regex, json_key) > 0) {
+        if (yr_re_match(context, key_regex, json_key) > 0) {
             return_integer(1);
         }
     }
@@ -73,28 +98,3 @@ begin_declarations;
 end_declarations;
 
 
-int module_initialize(YR_MODULE* module) {
-    return ERROR_SUCCESS;
-}
-
-
-int module_finalize(YR_MODULE* module) {
-    return ERROR_SUCCESS;
-}
-
-
-int module_load(YR_SCAN_CONTEXT* context, YR_OBJECT* module_object, void* module_data, size_t module_data_size) {
-    YR_MEMORY_BLOCK* block = first_memory_block(context);
-    uint8_t* block_data = block->fetch_data(block);
-
-    json_error_t json_error;
-    json_t* json = json_loads((const char*) block_data, 0, &json_error);
-    module_object->data = json;
-
-    return ERROR_SUCCESS;
-}
-
-
-int module_unload(YR_OBJECT* module_object) {
-    return ERROR_SUCCESS;
-}
